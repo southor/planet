ba48103a0d8cae34ca054d416a203b36a8deda7f
marcus.matern <marcus.matern@4a82640f-4124-0410-b267-737a41d4cac3>
2006-12-14 23:47:05 +0000

    Added virtual connection implementation

--- trunk/Prototype/src/virtualconnection.h
+++ trunk/Prototype/src/virtualconnection.h
@@ -1,25 +1,64 @@
 #ifndef __virtualconnection_h__
 #define __virtualconnection_h__
 
+#include <deque>
+
 #include "messages.h"
 
 namespace Prototype
 {
 	class VirtualConnection
 	{
-	protected:
+	public:
 		
 		class VirtualMessageSender : public MessageSender
-		{};
+		{
+		public:
+			VirtualMessageSender() : MessageSender() { }
+			~VirtualMessageSender();
 		
+			void setMessageDeque(std::deque<Message> *messageDeque);
+
+			void pushMessage(const Message &message);
+
+			// Get number of messages queued to be transmitted.
+			int getNMessages();
+			
+			// Transmits queued messages.
+			void transmit();
+					
+		private:
+			std::deque<Message> *messageDeque;
+		};
+
 		class VirtualMessageReciever : public MessageReciever
-		{};
+		{
+		public:
+			VirtualMessageReciever() : MessageReciever() { }
+			~VirtualMessageReciever();
+
+			void setMessageDeque(std::deque<Message> *messageDeque);
+
+			// Get number of messages recieved.
+			int getNMessages();		
+			
+			Message popMessage();
+		
+		private:
+			std::deque<Message> *messageDeque;
+		};
 
 	public:
 		VirtualConnection();
 
 		MessageSender* getMessageSender();
 		MessageReciever* getMessageReciever();
+
+	private:
+		std::deque<Message> messageDeque;
+		
+		VirtualMessageSender virtualMessageSender;
+		VirtualMessageReciever virtualMessageReciever;
 	};
 };
 
