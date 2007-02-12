115c2236cda9b4f98d7c6fe8d04f4db5ed1edf35
t.soderberg8 <t.soderberg8@4a82640f-4124-0410-b267-737a41d4cac3>
2007-02-12 00:06:52 +0000

    Started on the new "keyhandler"

---
+++ trunk/Prototype/src/Cmds.h
@@ -0,0 +1,57 @@
+#ifndef __Cmds_h__
+#define __Cmds_h__
+
+namespace Prototype
+{
+	namespace Cmds
+	{
+
+		// State Commands
+		enum
+		{
+			// Used by server and client
+			LEFT,
+			RIGHT,
+			FORWARD,
+			BACKWARD,
+			
+			// Only used by client
+			ROTATE_LEFT,
+			ROTATE_RIGHT,	
+		};
+
+		//Action commands
+		enum
+		{
+			SHOOT,
+			SWITCH_WEAPON
+		};
+
+		static const int N_STATE_CMDS = 6;
+		static const int N_ACTION_CMDS = 2;
+
+		//static const int N_CMDS = 8; // all commands
+		//static const int N_SERVER_STATE_CMDS = 4; // first group of commands
+		//static const int N_STATE_CMDS = 6; // first and second group of commands
+		//static const int N_ACTION_CMDS = 2; // third group of commands
+
+		//inline bool isCommand(int cmd)
+		//{
+		//	return (cmd >= 0) && (cmd < N_CMDS);
+		//}
+
+		//inline bool isStateCommand(int cmd)
+		//{
+		//	return (cmd >= 0) && (cmd < N_STATE_CMDS);
+		//}
+
+		//inline bool isActionCommand(int cmd)
+		//{
+		//	cmd -= N_STATE_CMDS;
+		//	return (cmd >= 0) && (cmd < N_ACTION_CMDS);
+		//}
+	};
+};
+
+
+#endif
