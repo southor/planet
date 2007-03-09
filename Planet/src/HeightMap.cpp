b6e93faf02213ac967201e1a8bcf8a760dd234d8
marcus.matern <marcus.matern@4a82640f-4124-0410-b267-737a41d4cac3>
2007-03-09 15:34:25 +0000

    Added heightmap for planet faces.

---
+++ Planet/src/HeightMap.cpp
@@ -0,0 +1,49 @@
+#include "HeightMap.h"
+
+#define WORLD_SIZE 30
+
+namespace Planet
+{
+	void HeightMap::init(char *image)
+	{
+		map = IMG_Load(image);
+		if (!map)
+		{
+			printf("IMG_Load: %s\n", IMG_GetError());
+		}
+
+		width = map->w;
+		depth = map->h;
+
+		floorHeights = new float[width * depth];
+
+		unsigned char* pixels = (unsigned char*)map->pixels;
+		int pitch = map->pitch;
+		int mapbpp = map->format->BytesPerPixel;
+
+		for (int z = 0; z < depth; z++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				double floorDepth = (pixels[x * mapbpp + z * pitch + 0] / 255.0f);
+
+				floorHeights[x + z * width] = (float)(-floorDepth);
+			}
+		}
+	}
+	
+	float HeightMap::getHeight(float s, float t)
+	{
+		int z = s * depth;
+		int x = t * width;
+		
+		return floorHeights[x + z * width];
+	}
+
+	HeightMap::~HeightMap()
+	{
+		SDL_FreeSurface(map);
+
+		delete [] floorHeights;
+	}
+};
